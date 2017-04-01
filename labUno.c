8be887db2bd5e5ebd0f26c572bf677d29f8e4dbd
Marcela <marcela.rivera.c@usach.com>
2017-04-01 17:42:54 -0300

    creando procesos

--- labUno.c
+++ labUno.c
@@ -4,27 +4,38 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define NFORKS 10
+
 
 void do_nothing(){
-	sleep(10); // 10 second
+	sleep(1); // 10 second
 }
 
 int main(){
 	int pid, j, status;
+	int pidP; 
+	int NFORKS  = 5;
 	for (j=0; j < NFORKS; j++){
-		if ((pid = fork()) < 0){
+		pid = fork();
+		pidP = getppid();
+		if(pidP>0 && pid>0)
+		{
+			if ((pid = fork()) < 0)
+			{
 			printf("no se pudo crear hijo\n");
 			exit(1);
-		}
+			}
 
-		if (pid == 0){ // hijo
-		    do_nothing();
-			exit(0);
-		}else{ // padre
+			if (pid == 0)
+			{ // hijo
+		    	do_nothing();
+				exit(0);
+			}
+		/*else{ // padre
 			waitpid(pid, &status, 0);
 			printf("%d\n", pid);
 			exit(0);
+		}*/
 		}
+		
 	}
 }
