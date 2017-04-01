64d13656bbe682d85ebd113408afd3028ac2df0c
Marcela <marcela.rivera.c@usach.com>
2017-04-01 16:31:11 -0300

    ejemplo dos de fork, hecho por el profe

--- labUno.c
+++ labUno.c
@@ -1,2 +1,30 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define NFORKS 10
+
+void do_nothing(){
+	sleep(10); // 10 second
+}
+
+int main(){
+	int pid, j, status;
+	for (j=0; j < NFORKS; j++){
+		if ((pid = fork()) < 0){
+			printf("no se pudo crear hijo\n");
+			//exit(1);
+		}
+
+		if (pid == 0){ // hijo
+		    do_nothing();
+			exit(0);
+		}else{ // padre
+			waitpid(pid, &status, 0);
+			printf("%d\n", pid);
+			exit(0);
+		}
+	}
+}
